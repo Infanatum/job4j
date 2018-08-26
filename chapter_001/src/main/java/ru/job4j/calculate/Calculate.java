67e0fa2165d63bef7f7ac0cd87120e01c12f30b1
Infanatum <infanatum@gmail.com>
2018-08-26 13:19:06 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,19 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Calculate is intended for output some text
+ * @author	Infanatum
+ * @since	26.08.2018
+ * @version	1
+ */
+
 public class Calculate {
-	
-	public static void main(String[] args) {
+		/**
+		 * Main
+		 * @param args - args
+		 */
+
+		public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
-
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Infanatum (mailto:infanatum@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+
+package ru.job4j.calculate;
