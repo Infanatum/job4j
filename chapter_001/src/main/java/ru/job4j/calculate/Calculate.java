a67ad12cbffcd70746bab238c00fd58205389a1b
Infanatum <infanatum@gmail.com>
2018-08-26 22:04:18 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,14 +6,21 @@ package ru.job4j.calculate;
  * @since	26.08.2018
  * @version	1
  */
-
 public class Calculate {
-		/**
-		 * Main
-		 * @param args - args
-		 */
+	/**
+	 * Main
+	 * @param args - args
+	 */
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+	}
 
-		public static void main(String[] args) {
-		System.out.println("Hello World");
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
 	}
 }
