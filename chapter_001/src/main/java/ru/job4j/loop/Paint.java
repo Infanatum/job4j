05c769dc49e2da466a3de897573a51346b24286a
Infanatum <infanatum@gmail.com>
2018-08-30 00:38:32 +0300

    5.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Class Paint draws objects.
  *
@@ -9,53 +11,16 @@ package ru.job4j.loop;
  */
 public class Paint {
     /**
-     * Drawing a pyramid.
-     *
-     * @param height Height.
-     */
-    public String pyramid(int height) {
-        StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
-    }
-
-    /**
      * Drawing a right triangle.
      *
      * @param height Height.
      */
     public String rightTrl(int height) {
-        // Буфер для результата.
-        StringBuilder screen = new StringBuilder();
-        // ширина будет равна высоте.
-        int weight = height;
-        // внешний цикл двигается по строкам.
-        for (int row = 0; row != height; row++) {
-            // внутренний цикл определяет положение ячейки в строке.
-            for (int column = 0; column != weight; column++) {
-                // если строка равна ячейки, то рисуем галку.
-                // в данном случае строка определяем, сколько галок будет на строке
-                if (row >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            // добавляем перевод строки.
-            screen.append(System.lineSeparator());
-        }
-        // Получаем результат.
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
     }
 
     /**
@@ -64,11 +29,38 @@ public class Paint {
      * @param height Height.
      */
     public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
+    /**
+     * Drawing a pyramid.
+     *
+     * @param height Height.
+     */
+    public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    /**
+     * Logic of drawing.
+     *
+     * @param height Height.
+     * @param weight Weight.
+     * @param predict Condition.
+     */
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int weight = height;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != weight; column++) {
-                if (row >= weight - column - 1) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
