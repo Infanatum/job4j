07c1a26d76bd8c6a869e4814b4a9a1e59919eb55
Infanatum <infanatum@gmail.com>
2018-08-31 23:03:35 +0300

    6.0. Заполнить массив степенями чисел. [#33488]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+/**
+ * Class Square is intended to fill an array with the square of the numbers 1 to n.
+ *
+ * @author Infanatum
+ * @version 1
+ * @since 31.08.2018
+ */
+public class Square {
+    /**
+     * Filling an array with the square of the numbers 1 to n
+     *
+     * @param bound N.
+     * @return Array.
+     */
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int i = 0; i < bound; i++) {
+            rst[i] = (int) Math.pow(i + 1, 2);
+        }
+        return rst;
+    }
+}
