4131de2e97574cd86d1e68c92bab2d10320c8793
Infanatum <infanatum@gmail.com>
2018-09-01 20:18:24 +0300

    6.2. Перевернуть массив[#69324] edits

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -4,32 +4,22 @@ package ru.job4j.array;
  * Class Turn is intended to reverse an array.
  *
  * @author Infanatum
- * @version 1
+ * @version 2
  * @since 01.09.2018
  */
 public class Turn {
     /**
-     * Finding the target element in an array.
+     * Reversing an array.
      *
      * @param array Source array.
      * @return Reversed array.
      */
     public int[] turn(int[] array) {
-        /**
-         * Reversing an array.
-         *
-         * @param array Source array.
-         * @return Reversed array.
-         */
-        int middle = array.length / 2;
-        int i = 0, j = array.length - 1, temp = 0;
-
-        for (; i < middle; i++, j--) {
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
         }
-
         return array;
     }
 }
